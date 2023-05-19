37e9489d0629c7c4d69974429646ecc4ab630478
keaguirre <ke.aguirre@duocuc.cl>
2023-05-18 23:41:12 -0400

    Se completa el registro del totem, linux y windows

--- devBlog.md
+++ devBlog.md
@@ -2,11 +2,22 @@
 - Comienza el desarrollo con electronjs
 
 # 16/04/23
-- [x] Obtener la mac(investigar segun plataforma y obtenerla segun ella con un if)
-- [x] Pasarle la mac al metodo de la api para poder llamar al detalle segun la mac
-- [x] Se consulta a la api sin problemas
-- [x] resolucion de respuestas con la api
+- [x] Obtener la mac(investigar segun plataforma y obtenerla segun ella con un if).
+- [x] Pasarle la mac al metodo de la api para poder llamar al detalle segun la mac.
+- [x] Se consulta a la api sin problemas.
+- [x] resolucion de respuestas con la api.
+
+# 18/04/23
+- [x] Asignarse el número de totem.
+- [x] Registrarse, si su mac no está registrada en el sistema (Dependiendo de la respuesta de registro, volverá a consultar o se asignará el número de totem).
+- Se cumple el proceso también para windows(sin probar).
 
 # To do:
-- [ ] Asignarse el número de totem.
-- [ ] Registrarse, si su mac no está registrada en el sistema (Dependiendo de la respuesta de registro, volverá a consultar o se asignará el número de totem).
+- Probar el sistema en windows.
+- Crear una base de datos local con sqlite
+- Consulte a la api con la mac propia y que localmente la guarde en sqlite
+- Al consultar a la api, si los datos coinciden ok.
+- Si los datos no coinciden, se actualizarán siguiendo los registros de la api.
+- Si existe la posibilidad de que la pagina esta funcionando pero el bloque de local no
+    generar una tabla de provisión que registrará la mac del totem y al numero autoasignado.
+    - Si este evento se gatilla, se generará un registro en una tabla de errores para mantener la integridad de los datos y boletas generadas en ese lapsus de tiempo.
--- main.js
+++ main.js
@@ -3,6 +3,13 @@ const os = require('os');
 const si = require('systeminformation');
 const axios = require('axios');
 const { exec } = require('child_process');
+const { app } = require('electron');
+const getmac = require('getmac');
+//Vars-------------
+let objConsulta; //contiene el objeto resultante de la consulta, mac, num_totem, id_local
+let macPC;
+var numTotem;
+const url = 'http://localhost:8000/api/totem/';
 
 //npm Start-------------------------------------
 const createWindow = () => {
@@ -14,29 +21,48 @@ const createWindow = () => {
     // win.loadURL('http://localhost:4200/admin/home')
 }
 
+
 app.on('ready', () => { //sincrono carga lo necesario para levantar la app
 });
+
 app.whenReady().then(async() => { //asincrono
-    const url = 'http://localhost:8000/api/totem/';
 
     const plataforma = await obtenerPlataforma();
     if(plataforma.includes('win32')){
-        console.log('SO: windo')
+        const macWindows = await obtenerDireccionMAC('win32');
+        const respuesta = await axios.get(url + macLinux).then(response =>{
+            objConsulta = response.data;
+            numTotem = response.data.num_totem; //almacenar en una tabla de sqlite el numtotem con la mac, si la que entrega de respuesta, coincide con la local, go
+        })
+        .catch(function(error) {
+            console.log('error, se procederá a registro del totem.');
+            console.log('mac: ',macWindows);
+            registrarTotem(macPC);
+
+        });
     }else if(plataforma.includes('linux')){
-        
-        const direccionesMAC = await obtenerDireccionMAC();
-        // const respuesta = await axios.get(url + direccionesMAC)
-        const respuesta = await axios.get(url + direccionesMAC).then(response=>{
-            console.log('dat: ', response.data) //Con esta data se debera asignarse a si mismo el numero de totem a la memoria o un archivo(?)
+
+        const macLinux = await obtenerDireccionMAC('linux');
+        const respuesta = await axios.get(url + macLinux).then(response =>{
+            objConsulta = response.data;
+            numTotem = response.data.num_totem; //almacenar en una tabla de sqlite el numtotem con la mac, si la que entrega de respuesta, coincide con la local, go
         })
         .catch(function(error) {
             console.log('error, se procederá a registro del totem.');
             //Una vez registrado, esperará la respuesta y si le devuelve el objeto nuevo creado, se asignará automáticamente su número de totem.
             //Si no devuelve el obj nuevo creado, entonces, con su mac, volverá a hacer la consulta para asignarse automáticamente un número de totem.
-        });
-        console.log('mac: ', direccionesMAC)
-    }
+            console.log('mac: ',macLinux);
+            registrarTotem(macPC);
 
+            /** @experimental
+             * Una de las posibilidades es que por x motivo, no tengamos acceso a la api pero si a la web que contiene angular, so es posible
+                levantar una instancia de sqlite3 y autoregistrarse localmente?
+                https://fmacedoo.medium.com/standalone-application-with-electron-react-and-sqlite-stack-9536a8b5a7b9
+             */
+        });
+        console.log('objResp: ', objConsulta)
+        // console.log('macPC: ', macPC)
+    }
     createWindow()
 })
 app.on('window-all-closed', () => {
@@ -50,15 +76,41 @@ function obtenerPlataforma() {
     });
 }
 
-function obtenerDireccionMAC() {
+function obtenerDireccionMAC(cod) { //usar un codigo de entrada con un parametro para un switch y dividir el metodo segun plataforma y asi reutilizo el mismo metodo?
     return new Promise((resolve, reject) => {
-        exec('ifconfig | grep -oE "([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}"', (error, stdout) => {
-            if (error) {
-                reject(error);
-            } else {
-                const direccionesMAC = stdout.trim().split('\n');
-                resolve(direccionesMAC);
+        switch(cod){
+            case 'linux': {
+                exec('ifconfig | grep -oE "([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}"', (error, stdout) => {
+                    if (error) {reject(error);
+                    }else {
+                        macPC = stdout.match(/([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}/)[0];
+                        console.log('macPC: ',macPC)
+                        resolve(macPC);
+                    }
+                });
             }
-        });
+            case('win32'): {
+                getmac.getMac((err, macPC) => {
+                    if (err) {reject(err);
+                    }resolve(macPC);
+                });
+            }
+        }
+    });
+}
+
+/**
+ * Registra un totem en el servidor.
+ * @param {string} mac - Dirección MAC del totem.
+ * @returns {Promise} - Promesa que se resuelve con la respuesta del servidor o se rechaza con el error.
+ */
+function registrarTotem(mac){
+    axios.post(url, { "mac_totem": mac, "local_asignado": 1, "estado": true})
+    .then(function (response) {
+
+        console.log('resp: ', response.data);
+    })
+    .catch(function (error) {
+        console.log('err: ',error);
     });
 }
